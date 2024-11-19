6fe5764325a519c082338f305f38e312dce0f6e5
prolley-parnell <prolley-parnell@gmail.com>
2024-11-19 10:28:38 +0000

    First commit

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#Run this from device connected to drive with data
+USER=$1 #sXXXXXXX
+input_folder=$2 #Path to the /data folder that contains an input directory that is not zipped
+PROJECT_NAME=sleap
+afs_project_path=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME} #The s20 is the first two numbers of the user student number
+
+echo "You are on branch afs so are setting up the AFS."
+echo "Press Ctrl+C if this is not correct and switch to the correct branch."
+echo ${USER}
+
+
+#Make the project path in AFS if it does not exist already
+if [ ! -d "${afs_project_path}" ]; then
+  mkdir -p ${afs_project_path}
+fi
+
+#If there is no data/input folder in the project folder, then make it.
+afs_src="${afs_project_path}/data/input"
+
+if [ ! -d ${afs_src} ]; then
+  mkdir -p ${afs_src}
+fi
+
+if [ ! -f "${input_folder}/input.tar.bz2" ]; then
+  cd ${input_folder}
+  tar --no-xattrs --exclude="._*" -cjf input.tar.bz2 input
+  mv input.tar.bz2 ${afs_src}
+fi
+
