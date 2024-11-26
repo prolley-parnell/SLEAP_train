ecd32734617098ab2f7354060fe0e8e71aed6a55
prolley-parnell <prolley-parnell@gmail.com>
2024-11-26 14:28:48 +0000

    remove inference confidence limit

--- inference_job.sh
+++ inference_job.sh
@@ -5,9 +5,9 @@ models_path=$3 #${DFS_HOME}/${PROJECT_NAME}/data/models
 
 sleap-track \
 -m "${models_path}/25112024_124117.centered_instance" \
--m "${models_path}/25112024_124117.centroid" \
+-m "${models_path}/25112024_124136.centroid" \
 --no-empty-frames \
 --verbosity rich \
 --batch_size 16 \
 --tracking.tracker simple \
-"${scratch_input}/*"
+"${scratch_input}/"
