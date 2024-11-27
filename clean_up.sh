627da418d440ab54b8490e223f3b5a784d442994
prolley-parnell <prolley-parnell@gmail.com>
2024-11-27 15:39:21 +0000

    update naming for afs and dfs paths

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 PROJECT_NAME=sleap
 project_path=/home/${USER}/${PROJECT_NAME}
-afs_src=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/input #s20 is the first two digits of the student number given in $USER
 
 echo "You are cleaning up, this will delete any output files left in the disk scratch spaces after running rsync"
 echo "Do you wish to continue? [y/n]"
@@ -11,33 +10,33 @@ if [ ! "$response" = "y" ] ; then
   exit
 fi
 
-dfs_src="${project_path}/data/output"
-afs_dst="/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/output"
+dfs_output_path="${project_path}/data/output"
+afs_output_path="/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/output"
 
 
 #Make the data output path if it is not present
-if [ ! -d ${afs_dst} ]; then
-  mkdir -p ${afs_dst}
+if [ ! -d ${afs_output_path} ]; then
+  mkdir -p ${afs_output_path}
 fi
 
 #Synchronise the folders and move the AFS input to DFS input
-rsync --archive --update --compress --progress ${dfs_src}/ ${afs_dst}
-echo "${dfs_src}/ up to date with ${afs_dst}"
+rsync --archive --update --compress --progress ${dfs_output_path}/ ${afs_output_path}
+echo "${dfs_output_path}/ up to date with ${afs_output_path}"
 
-rm -rf ${dfs_src}
+rm -rf ${dfs_output_path}
 
 ## ------ move model files across ------ ##
-dfs_src="${project_path}/data/models"
-afs_dst="/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/models"
+dfs_models_path="${project_path}/data/models"
+afs_models_path="/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/models"
 
 
 #Make the data output path if it is not present
-if [ ! -d ${afs_dst} ]; then
-  mkdir -p ${afs_dst}
+if [ ! -d ${afs_models_path} ]; then
+  mkdir -p ${afs_models_path}
 fi
 
 #Synchronise the folders and move the AFS input to DFS input
-rsync --archive --update --compress --progress ${dfs_src}/ ${afs_dst}
-echo "${dfs_src}/ up to date with ${afs_dst}"
+rsync --archive --update --compress --progress ${dfs_models_path}/ ${afs_models_path}
+echo "${dfs_models_path}/ up to date with ${afs_models_path}"
 
-rm -rf ${dfs_src}
+rm -rf ${dfs_models_path}
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #USER=$1 #sXXXXXXXX
 PROJECT_NAME=sleap
-afs_src=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/input #s20 is the first two digits of the student number given in $USER
-afs_model_src=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/models #s20 is the first two digits of the student number given in $USER
+afs_input_path=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/input #s20 is the first two digits of the student number given in $USER
+afs_model_path=/afs/inf.ed.ac.uk/user/s20/${USER}/${PROJECT_NAME}/data/models #s20 is the first two digits of the student number given in $USER
 
 echo "You are on branch dfs so are setting up the DFS."
 echo "Press Ctrl+C if this is not correct and switch to the correct branch."
@@ -17,43 +17,43 @@ if [ ! -d "${conda_path}" ]; then
   conda config --set auto_activate_base false
 fi
 
-project_path=/home/${USER}/${PROJECT_NAME}
+dfs_project_path=/home/${USER}/${PROJECT_NAME}
 
 #Make the project path if it is not present
-if [ ! -d "${project_path}" ]; then
-  mkdir -p ${project_path}
+if [ ! -d "${dfs_project_path}" ]; then
+  mkdir -p ${dfs_project_path}
 fi
 
-dfs_dst="${project_path}/data/input"
+dfs_input_path="${dfs_project_path}/data/input"
 
 #Make the data input path if it is not present
-if [ ! -d ${dfs_dst} ]; then
-  mkdir -p ${dfs_dst}
+if [ ! -d ${dfs_input_path} ]; then
+  mkdir -p ${dfs_input_path}
 fi
 
 #Synchronise the folders and move the AFS input to DFS input
-rsync --archive --update --compress --progress ${afs_src}/ ${dfs_dst}
+rsync --archive --update --compress --progress ${afs_input_path}/ ${dfs_input_path}
 
-echo "${afs_src}/ up to date with ${dfs_dst}"
+echo "${afs_input_path}/ up to date with ${dfs_input_path}"
 
 # --- Move models from AFS --- #
-dfs_model_dst="${project_path}/data/models"
+dfs_model_path="${dfs_project_path}/data/models"
 
 #Make the model input path if it is not present
-if [ ! -d ${dfs_model_dst} ]; then
-  mkdir -p ${dfs_model_dst}
+if [ ! -d ${dfs_model_path} ]; then
+  mkdir -p ${dfs_model_path}
 fi
 
 #Synchronise the folders and move the AFS models to DFS models
-rsync --archive --update --compress --progress ${afs_model_src}/ ${dfs_model_dst}
+rsync --archive --update --compress --progress ${afs_model_path}/ ${dfs_model_path}
 
-echo "${afs_model_src}/ up to date with ${dfs_model_dst}"
+echo "${afs_model_path}/ up to date with ${dfs_model_path}"
 
 #Install SLEAP through conda
 source ~/.bashrc
 
 if { ! conda env list | grep 'sleap'; } >/dev/null 2>&1; then
-  TMPDIR="${project_path}/tmp"
+  TMPDIR="${dfs_project_path}/tmp"
   TMP="${TMPDIR}"
   TEMP="${TMPDIR}"
   mkdir -p "${TMPDIR}"
@@ -65,9 +65,9 @@ fi
 
 #Ensure that you can use the "run_experiments" wrapper
 #echo 'export PATH=/home/$USER/cluster-scripts/experiments:$PATH' >> ~/.bashrc
-if [ -f "${dfs_dst}/input.tar.bz2" ]; then
-  tar --exclude="._*" -xjvf "${dfs_dst}/input.tar.bz2" -C "${project_path}/data"
-  rm -rf "${dfs_dst}/input.tar.bz2"
+if [ -f "${dfs_input_path}/input.tar.bz2" ]; then
+  tar --exclude="._*" -xjvf "${dfs_input_path}/input.tar.bz2" -C "${dfs_project_path}/data"
+  rm -rf "${dfs_input_path}/input.tar.bz2"
 else
-  echo "Could not find '${dfs_dst}/input.tar.bz2'"
+  echo "Could not find '${dfs_input_path}/input.tar.bz2'"
 fi
