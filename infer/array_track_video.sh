e4660f82db9281801337a295ab9ef02c761f91e6
prolley-parnell <prolley-parnell@gmail.com>
2025-02-11 16:40:32 +0000

    Adding folder structure and experiment scripts

--- infer/array_track_video.sh
+++ infer/array_track_video.sh
@@ -42,7 +42,8 @@
 #SBATCH --gres=gpu:1
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
-#SBATCH --mem=23000
+# NODE_MAX_MEM - 1000 / N_GPU = (515000 - 1000) / 4
+#SBATCH --mem=128750
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
 #SBATCH --cpus-per-task=1
